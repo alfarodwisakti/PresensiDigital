82471906692db09a3e110ddf69ecb133eab8f911
Alfaro dwi sakti <alfarodwisakkti@gmail.com>
2026-09-25 17:26:08 +0700

    y

--- apps-script/postinstall.cjs
+++ apps-script/postinstall.cjs
@@ -1,10 +1,10 @@
-   const fs = require('fs');
-   const path = require('path');
-   const binDir = path.join(__dirname, '..', 'node_modules', '.bin');
-   try {
-     if (fs.existsSync(binDir)) {
-       for (const f of fs.readdirSync(binDir)) {
-         try { fs.chmodSync(path.join(binDir, f), 0o755); } catch (_) {}
-       }
-     }
-   } catch (_) {}
+const fs = require('fs');
+const path = require('path');
+const binDir = path.join(__dirname, '..', 'node_modules', '.bin');
+try {
+  if (fs.existsSync(binDir)) {
+    for (const f of fs.readdirSync(binDir)) {
+      try { fs.chmodSync(path.join(binDir, f), 0o755); } catch (_) {}
+    }
+  }
+} catch (_) {}
